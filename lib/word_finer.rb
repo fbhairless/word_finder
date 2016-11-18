0ab67e090dba420bd5469ba261898a6ba8d8b6d8
Hal Fairless <hbfairless@gmail.com>
2016-11-18 08:39:26 -0800

    this commit is the initial file structure

---
+++ lib/word_finer.rb
@@ -0,0 +1,16 @@
+
+class String
+  define_method(:word_finder) do
+    titlecased_words = []
+    special_words = ["to", "the", "of", "from", "and", "but", "or", "by", "on"]
+    words = self.split()
+    words.each do |word|
+      if (word != words.at(0)) && (special_words.include?(word))
+        titlecased_words.push(word)
+      else
+        titlecased_words.push(word.capitalize)
+      end
+    end
+    titlecased_words.join(" ")
+  end
+end
---
+++ spec/title_case_integration_spec.rb
@@ -0,0 +1,14 @@
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exception, false)
+
+describe('the word finder', {:type => :feature}) do
+  it('accepts a word and a phrase, then counts the occurances of the word in the phrase ') do
+    visit('/')
+    fill_in('word', :with => 'input word')
+    fill_in('phrase', :with => 'input phrase')
+    click_button('Send')
+    expect(page).to have_content('word and word count fron phrae')
+  end
+end
